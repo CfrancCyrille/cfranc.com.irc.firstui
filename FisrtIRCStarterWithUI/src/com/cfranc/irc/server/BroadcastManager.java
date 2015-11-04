408d3a5d693a9809f4fb2619335ea85fcd829eaf
Cyrille FRANCOIS <francois.cyrille@gmail.com>
2015-11-04 15:35:11 +0100

    Renommage en un gestionnaire de broadcast

---
+++ FisrtIRCStarterWithUI/src/com/cfranc/irc/server/BroadcastManager.java
@@ -0,0 +1,48 @@
+package com.cfranc.irc.server;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class BroadcastManager{
+	
+	public static HashMap<User, ServerToClientThread> clientTreadsMap=new HashMap<User, ServerToClientThread>();
+	static{
+		Collections.synchronizedMap(clientTreadsMap);
+	}
+	
+	public static boolean addClient(User user, ServerToClientThread serverToClientThread){
+		boolean res=true;
+		if(clientTreadsMap.containsKey(user)){
+			res=false;
+		}
+		else{
+			serverToClientThread.start();
+			clientTreadsMap.put(user, serverToClientThread);
+		}
+		return res;
+	}
+
+	public static void sendMessage(User sender, String msg){
+		Collection<ServerToClientThread> clientTreads=clientTreadsMap.values();
+		Iterator<ServerToClientThread> receiverClientThreadIterator=clientTreads.iterator();
+		while (receiverClientThreadIterator.hasNext()) {
+			ServerToClientThread clientThread = (ServerToClientThread) receiverClientThreadIterator.next();
+			clientThread.post("#"+sender.getLogin()+"#"+msg);			
+			System.out.println("sendMessage : "+"#"+sender.getLogin()+"#"+msg);
+		}
+	}
+	
+	public static void removeClient(User user){
+		clientTreadsMap.remove(user);
+	}
+	
+	public static boolean accept(User user){
+		boolean res=true;
+		if(clientTreadsMap.containsKey(user)){
+			res= false;
+		}
+		return res;
+	}
+}
